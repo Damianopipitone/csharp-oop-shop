3807930e955a5a601fd79eb16c0a8b4a33880caa
Damianopipitone <damianopipitone33@gmail.com>
2023-04-18 16:40:45 +0200

    Aggiungere i file di progetto.

---
+++ shop/Class1.cs
@@ -0,0 +1,55 @@
+﻿using System.Dynamic;
+
+namespace shop
+{
+    public class Product
+    {   
+        //ATTRIBUTI 
+        public string name;
+        public int code;
+        public string description;
+        float price;
+        int IVA;
+        
+        
+        //COSTRUTTORE
+        public Product(string name, int code, string description, float price, int IVA)
+        {
+            this.name = name;
+            this.code = code;
+            this.description = description;
+            this.price = price;
+            this.IVA = IVA;
+        }
+        //METODI   
+        public string nameProduct()
+        {
+            return "Il nostro prodotto si chiama " + name;
+
+        }
+        
+        public int codeProduct()
+        {
+            return code;
+        }
+
+        public string descriptionProduct()
+        {
+            return description;
+        }
+
+        public float priceProduct()
+        {
+            return price;
+        }
+
+        public int IVAProduct()
+        {
+            return IVA;
+        }
+        public void setName (string name)
+        {
+            this.name = name; 
+        }
+    }
+}
---
+++ shop/Program.cs
@@ -0,0 +1,13 @@
+﻿using shop;
+
+Product oggettoMaglia = new Product("Maglia blu", 0123456, "Prodotto tessuto al 100% da cotone", 29.99f, 22);
+
+Console.WriteLine(oggettoMaglia.nameProduct());
+
+Console.WriteLine(oggettoMaglia.codeProduct());
+
+Console.WriteLine(oggettoMaglia.descriptionProduct());
+
+Console.WriteLine("Il prezzo del prodotto è di: " + oggettoMaglia.priceProduct());
+
+Console.WriteLine("L'IVA è del " + oggettoMaglia.IVAProduct() + "%");
